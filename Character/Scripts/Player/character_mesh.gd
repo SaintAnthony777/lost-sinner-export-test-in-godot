412af95d0992d3db0373587336995eabf7870125
Anthony <radahyanthony@gmail.com>
2026-02-18 11:02:30 +0300

    feat: creating blocking stances

--- Character/Scripts/Player/character_mesh.gd
+++ Character/Scripts/Player/character_mesh.gd
@@ -8,8 +8,14 @@ var is_backfliping:bool=false
 var starts_backflips:bool=false
 var is_shouting:bool=false
 
+@onready var wing_mesh: MeshInstance3D = $"Armature/Skeleton3D/Wings_Attachement/Angel wings/Wing_Mesh"
+@onready var rune_mesh: MeshInstance3D = $Feet_bursts/angel_burst_rune/rune_mesh
+
 @onready var animation_tree: AnimationTree = $AnimationTree
 
+func _ready() -> void:
+	pass
+	props_preload()
 func normal_motion(current_action:String)->void:
 	grounding("Normal")
 	animation_tree.set("parameters/Normal_Transition/transition_request",current_action)
@@ -56,3 +62,11 @@ func starts_backflipping()->void:
 	starts_backflips=true
 func stops_backflipping()->void:
 	starts_backflips=false
+
+
+func props_preload()->void:
+	wing_mesh.show()
+	wing_mesh.hide()
+	rune_mesh.show()
+	rune_mesh.hide()
+	
