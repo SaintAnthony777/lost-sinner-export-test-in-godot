2d18250820a5fdd36dae4a1c6eeed88daa990bcd
Anthony <radahyanthony@gmail.com>
2026-03-06 18:21:48 +0300

    fix: cleaning shader codes

--- Character/Scripts/StateMachines/Extended_scripts/hammer_attack_4.gd
+++ Character/Scripts/StateMachines/Extended_scripts/hammer_attack_4.gd
@@ -3,7 +3,7 @@ extends State
 @onready var player: player_character = $"../.."
 
 func enter() -> void:
-	character.force_character_rotation()
+	character.force_character_rotation()	
 func physics_update(_delta) -> void:
 	attack_stuff()
 	attack_check()
