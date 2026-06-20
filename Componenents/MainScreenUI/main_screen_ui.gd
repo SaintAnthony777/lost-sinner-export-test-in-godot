22df39988478790dbbf2d8d007d6a8ffd8de906a
anthony radahy <radahyanthony@gmail.com>
2026-06-20 12:04:09 +0300

    feat:added options menu

--- Componenents/MainScreenUI/main_screen_ui.gd
+++ Componenents/MainScreenUI/main_screen_ui.gd
@@ -5,6 +5,10 @@ class_name MainscreenUI extends Control
 
 @onready var empty_data:PackedScene=preload("res://Componenents/Empty Data/empty_data.tscn")
 @onready var existing_data:PackedScene=preload("res://Componenents/Existing Data/existing_data.tscn")
+@onready var copyrights_margin_container: MarginContainer = $"MarginContainer/Copyrights Margin Container"
+@onready var main_buttons: VBoxContainer = $"MarginContainer/Main buttons"
+@onready var save_slots_and_descriptions: VBoxContainer = $"MarginContainer/Save slots and descriptions"
+@onready var options: MarginContainer = $MarginContainer/Options
 
 func _ready() -> void:
 	init_slots() 
@@ -21,3 +25,28 @@ func init_slots() -> void:
 			var emptydata:EmptyData=empty_data.instantiate()
 			emptydata.save_id=i
 			save_slots_row.add_child(emptydata)
+
+
+func _on_start_game_pressed() -> void:
+	reverse_show()
+
+
+func _on_back_pressed() -> void:
+	reverse_show()
+
+func reverse_show()->void:
+	save_slots_and_descriptions.visible=!save_slots_and_descriptions.visible
+	main_buttons.visible=!main_buttons.visible
+	copyrights_margin_container.visible=!copyrights_margin_container.visible
+
+func reverse_from_options()->void:
+	options.visible=!options.visible
+	main_buttons.visible=!main_buttons.visible
+	copyrights_margin_container.visible=!copyrights_margin_container.visible
+
+func _on_back_from_options_pressed() -> void:
+	reverse_from_options()
+
+
+func _on_options_pressed() -> void:
+	reverse_from_options()
